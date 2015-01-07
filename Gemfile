ec1c4fe61a035e36ca067d2acb71c8a9ad8a393e
George Pai <gpai0@yahoo.com>
2015-01-06 17:05:26 -0800

    Add image upload with Paperclip

--- Gemfile
+++ Gemfile
@@ -6,13 +6,13 @@ gem 'rails', '4.1.8'
 gem 'sass-rails', '~> 4.0.3'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.0.0'
-
 gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0',          group: :doc
 gem 'bootstrap-sass'
 gem 'devise', '~> 3.4.1'
+gem 'paperclip', github: 'thoughtbot/paperclip'
 
 group :development, :test do 
 	gem 'sqlite3'
--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -19,7 +19,8 @@ class PinsController < ApplicationController
   end
 
   def create
-    @pin = current_user.pins.build(pin_params)
+    @pin = current_user.pins.build(pin_params) 
+    
     if @pin.save
       redirect_to @pin, notice: 'Pin was successfully created.'
     else
@@ -47,13 +48,13 @@ class PinsController < ApplicationController
     end
 
     def correct_user
-      @pin=current_user.pins.find_by(params[:id])
+      @pin = current_user.pins.find_by(id: params[:id])
       redirect_to pins_path, notice: "Not autorized to edit this pin" if @pin.nil?
     end
 
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
-      params.require(:pin).permit(:description)
+      params.require(:pin).permit(:description, :image)
     end
 end
